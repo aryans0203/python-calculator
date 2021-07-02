7202b92810b7b32e7fdbf2953922169b1ef617ff
aryans0203 <86836096+aryans0203@users.noreply.github.com>
2021-07-02 17:58:11 +0530

    DAY-1 ASSIGNMENT

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a=int(input("input your first number/dividend:-"))
+b=int(input("input your second number/divisor:-"))
+Sum=a+b
+Diff=a-b
+Pro=a*b
+Divi=a/b
+print("the sum of the two numbers is:-",Sum)
+print("the difference of the two numbers is:-",Diff)
+print("the product of the two numbers is:-",Pro)
+print("the division of the two numbers is:-",Divi)
